a14e40c69a32ab19fc04ad50f10acbfefbfed253
Lathan Britz <jucallme@gmail.com>
2023-10-26 12:49:45 -0300

    no cert

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import mkcert from 'vite-plugin-mkcert'
 
-export default defineConfig({
-  plugins: [mkcert(), vue()],
-  server: {
-    https: true,
-    strictPort: true,
-    port: 3016,
-  },
-  define: {
-    'process.env': {}
-  }
-})
-
 // export default defineConfig({
-//   plugins: [vue()],
+//   plugins: [mkcert(), vue()],
+//   server: {
+//     https: true,
+//     strictPort: true,
+//     port: 3016,
+//   },
 //   define: {
 //     'process.env': {}
 //   }
 // })
 
+export default defineConfig({
+  plugins: [vue()],
+  define: {
+    'process.env': {}
+  }
+})
