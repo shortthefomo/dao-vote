312c416d0cf8416ccfb062ea37f47e141fb45a47
Lathan Britz <jucallme@gmail.com>
2023-10-26 12:41:34 -0300

    more files

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import mkcert from 'vite-plugin-mkcert'
+
+export default defineConfig({
+  plugins: [mkcert(), vue()],
+  server: {
+    https: true,
+    strictPort: true,
+    port: 3016,
+  },
+  define: {
+    'process.env': {}
+  }
+})
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   define: {
+//     'process.env': {}
+//   }
+// })
+
