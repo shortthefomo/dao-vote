760a4904151ec7e23661f4d99943e1c256307935
Lathan Britz <jucallme@gmail.com>
2023-10-29 17:11:51 -0300

    dev

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -16,7 +16,10 @@
                     Ledger Index: {{ validator_data.ledger_index }}
                 </p>
                 <p v-if="validator_data !== null" class="text">
-                    Full: {{ validator_data.full }}
+                    Base Fee: {{ validator_data.base_fee }}
+                </p>
+                <p v-if="validator_data !== null" class="text">
+                    Load Fee: {{ validator_data.load_fee }}
                 </p>
                 <p v-if="validator_data !== null" class="text">
                     State: {{ validator_data.state }}
