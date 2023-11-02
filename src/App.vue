21463e6233528f58f0469e2058f6134130b2d41e
Lathan Britz <jucallme@gmail.com>
2023-11-01 22:14:43 -0300

    dev

--- src/App.vue
+++ src/App.vue
@@ -96,6 +96,19 @@
                 await this.jwtSignIn()
                 this.currentLedger()
             },
+            // async getUUID(uuid) {
+            //     const {data} = await this.axios.get(`https://vote-backend.panicbot.xyz/api/v1/apps/payload_uuid?appkey=${import.meta.env.VITE_XUMM_APPKEY}&uuid=${uuid}`)
+            //     console.log('heyy 22222')
+            //     console.log(data)
+            //     if ('response' in data && 'user' in data.response) {
+            //         this.$store.dispatch('setUserUUID', data.response.user)
+            //         this.$store.dispatch('setUserToken', data.application.issued_user_token)
+            //         // console.log('Set User Token', data.response.user)
+            //     }
+            //     const payload = await this.Sdk.payload.get(uuid)
+            //     console.log('heyy 3333')
+            //     console.log(payload)
+            // },
             async jwtSignIn() {
                 const self = this
                 const request  = { txjson: { TransactionType: 'SignIn' }}
@@ -114,7 +127,7 @@
                         if ('response' in data && 'user' in data.response) {
                             this.$store.dispatch('setUserUUID', data.response.user)
                             this.$store.dispatch('setUserToken', data.application.issued_user_token)
-                            // console.log('Set User Token', data.response.user)
+                            console.log('Set User Token', data.application.issued_user_token)
                         }
                         self.components.Landing = true
                         return event.data
